e7ae428ae1a53f7e13745b254f4b50da2dd12b9c
Sujith Sivasankaran <60563760+SujithSivasankaran@users.noreply.github.com>
2021-06-20 21:40:16 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,64 @@
+import requests
+import matplotlib.image as mpimg
+
+from flask import Flask, flash, request, redirect, url_for, render_template
+# import urllib.request
+import os
+from werkzeug.utils import secure_filename
+
+app = Flask(__name__)
+
+UPLOAD_FOLDER = 'static/uploads/'
+
+app.secret_key = "secret key"
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+
+ALLOWED_EXTENSIONS = set(['png','jpg'])
+
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/', methods=['POST'])
+def upload_image():
+    if 'file' not in request.files:
+        flash('No file part')
+        return redirect(request.url)
+    file = request.files['file']
+    if file.filename == '':
+        flash('No image selected for uploading')
+        return redirect(request.url)
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        # print('upload_image filename: ' + filename)
+        img = mpimg.imread(file)
+        img = img / 255
+        img = img.reshape(784)
+        input_data = "{\"data\": [" + str(list(img)) + "]}"
+        headers = {'Content-Type': 'application/json'}
+        service = 'http://3555b310-424e-4a8a-9bfb-a53d72f4cc9a.centralus.azurecontainer.io/score'
+        resp = requests.post(service, input_data, headers=headers)
+        m = resp.text
+        flash('The no is predicted to be: ' + m[1])
+        return render_template('index.html')
+    else:
+        flash('Allowed image types are - png, jpg, jpeg, gif')
+        return redirect(request.url)
+
+
+@app.route('/display/<filename>')
+def display_image(filename):
+    # print('display_image filename: ' + filename)
+    return redirect(url_for('static', filename='uploads/' + filename), code=301)
+
+
+if __name__ == "__main__":
+    app.run()
